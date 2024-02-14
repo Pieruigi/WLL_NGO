6f6589e9d6cfbd3aa495526f47a34593ea522b48
Pieruigi <pierluigi.zomparelli@gmail.com>
2024-02-14 16:27:00 +0100

    adding ball

---
+++ Assets/Scripts/NetCode/BallController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WLL_NGO.Netcode
+{
+    public class BallController : SingletonNetwork<BallController>
+    {
+        
+    }
+
+}
